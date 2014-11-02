130cf9d7e6ae5e8034d26f9f36035091ca761636
c1 <c1@localhost.localdomain>
2014-11-02 21:04:33 +0800

    my first project

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_world.c
+ *
+ *    Description:  print hello world
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月02日 20时51分48秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world!\n");
+	return 0;
+}
